5aa301391b9c48ad9a852e516302707d5a36f041
poplovk <legerzenit89@gmail.com>
2020-11-04 22:45:17 +0300

    upd 04.11 (2)

--- шифратор.py
+++ шифратор.py
@@ -1,5 +1,6 @@
 import os
 import time
+import random
 
 # print(type(choose))
 
@@ -11,7 +12,8 @@ while True:
     print("[---           1. Ввести код")
     print("[---           2. Загрузить данные")
     print("[---           3. Что это такое?")
-    print("[---           4. Выйти")
+    print("[---           4. Сыграть в Поле Чудес")
+    print("[---           5. Выйти")
     choose = int(input("[---  "))
 
     if choose == 1:
@@ -27,11 +29,11 @@ while True:
         for counter in range(len(truecode)):
             if truecode[counter] == inputcode:
                 time.sleep(1)
-                print("┬─┬")
+                print("┬─┬   "  "(3)")
                 time.sleep(1)
-                print("─┬")
+                print("─┬    "   "(2)")
                 time.sleep(1)
-                print("┬")
+                print("┬     "    "(1)")
                 time.sleep(1)
                 print("Done!")
                 time.sleep(1)
@@ -53,12 +55,69 @@ while True:
         print("Готово! Ваш код для просмотра: " + usercode)
         time.sleep(1)
     if choose == 3:
+        time.sleep(1)
         print("Q: Что-это?")
+        time.sleep(2)
         print("A: Своеобразная программа для хранения файлов. Анонимная.")
         print()
+        time.sleep(3)
         print("Q: Как это работает?")
+        time.sleep(2)
         print("A: Вы можетете загружать любые данные (кроме файлов напрямую) и присваивать им код, ")
         print("зная который любой может получить к ним доступ")
+        time.sleep(6)
     if choose == 4:
+
+        allwords = ["год", "человек", "время", "дело", "жизнь", "день", "рука", "слово", "место", "вопрос", "лицо",
+                    "глаз",
+                    "друг", "сторона", "дом", "ребенок", "голова", "система", "вид", "конец", "отношение", "город",
+                    "часть",
+                    "женщина", "проблема"]
+        print("-------------------------------------Поле Чудес-------------------------------------")
+        attempts = int(input("[---     Введите количество попыток: "))
+        word = (random.choice(allwords))
+
+        letters = []
+        counter = 0
+        flag = 0
+        allowedletters = ["а", "б", "в", "г", "д", "е", "ж", "з", "и", "й", "к", "л", "м", "н", "о", "п", "р", "с", "т",
+                          "у",
+                          "ф", "х", "ц", "ч", "ш", "щ", "ъ", "ы", "ь", "э", "ю", "я"]
+        while attempts > 0:
+            letter = input("[---     Введите букву: ")
+            if letter == word:
+                print("Слово отгадано! (>_<)")
+                break
+            if letter not in allowedletters:
+                continue
+            if letter in letters:
+                print("Вы повторяетесь")
+                print("----------------------------------------------------------------------------------------------------")
+
+            else:
+
+                letters.append(letter)
+                counter = 0
+                flag = False
+                for char in word:
+                    if char in letters:
+                        print(char)
+                        counter = counter + 1
+
+                    else:
+                        print("-")
+                        flag = True
+
+            if not flag:
+                print("Слово отгадано! (>_<)")
+                break
+            if letter not in word:
+                attempts = attempts - 1
+            if attempts == 0:
+                print("Вы проиграли (╯°□°）╯︵ ┻━┻")
+                print("[---     Правильное слово: " + word)
+            print("Возвращаемся в главное меню...")
+            time.sleep(3)
+    if choose == 5:
         print("Завершили работу >_<")
         break
